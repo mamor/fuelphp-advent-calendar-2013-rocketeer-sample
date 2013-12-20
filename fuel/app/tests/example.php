01c57438c05bd0b94e2c2d0d7a615d3746231ed7
mamor <mamor.dev@gmail.com>
2013-12-20 21:51:47 +0900

    必ず失敗するテストを追加した

---
+++ fuel/app/tests/example.php
@@ -0,0 +1,17 @@
+<?php
+
+/* Test_Example class
+ *
+ * @author    Mamoru Otsuka http://madroom-project.blogspot.jp/
+ * @copyright 2013 Mamoru Otsuka
+ * @license   WTFPL License http://www.wtfpl.net/txt/copying/
+ */
+class Test_Example extends Fuel\Core\TestCase
+{
+
+	public function test_fail()
+	{
+		$this->fail();
+	}
+
+}
